78aa7c0fee93446c948732db75b0a5616a7647a9
Sierra Curtis <sierracurtis91@gmail.com>
2020-05-18 16:49:44 -0400

    finished mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -11,6 +11,27 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    # figute out the total size of the arr
+    low = 0
+    high = len(arr) -1 
 
+    while low <= high:
+        # find the midpoint
+        midpoint = (low + high) // 2
+
+        # check to see if the midpoint element is our target
+        if arr[midpoint] == target:
+            return midpoint
+
+        # check to see if the element should be low or high of the midpoint
+        if arr[midpoint] < target:
+            # toss out the low of the arr
+            # update out `low` index
+            low = midpoint + 1
+
+        # otherwise, arr[mid] > target
+        else:
+            # toss out the high of the arr because the element has to be low
+            high = midpoint -1
 
     return -1  # not found
