be50489b039222363a6e1299b360be053d10a2b1
Sierra Curtis <sierracurtis91@gmail.com>
2020-05-18 16:19:41 -0400

    finished selection_sort and bubble_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,19 +7,13 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-        # for j in range(i, len(arr) - 1):
-        #     if arr[j] < arr[smallest_index]:
-        #         # arr[j] = arr[j -1]
-        #         smallest_index = j
-        while cur_index > 0 and arr[cur_index -1] > smallest_index:
-            arr[cur_index] = arr[cur_index -1]
-            cur_index = cur_index -1
-            
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
-        # arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
-        arr[cur_index] = smallest_index
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
     return arr
 
@@ -27,7 +21,19 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    # sort the arr
+    for i in range(1, len(arr)):
+        curr_bubble = arr[i]
+        j = i
+        # put curr_bubble in the appropriate spot in our sorted half
+        # loop through our sorted half and find the appropriate spot
+        while j > 0 and curr_bubble < arr[j - 1]:
+            # taking the j - 1th bubble and copying it over to the jth spot
+            # arr[j], arr[j -1] = arr[j - 1], arr[j]
+            arr[j] = arr[j - 1]
+            j -= 1
+        # insert the bubble at the correct position
+        arr[j] = curr_bubble
 
     return arr
 
