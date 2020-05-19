6ad886fae7cd1ef9dacde6cd932e4730c57b692c
Sierra Curtis <sierracurtis91@gmail.com>
2020-05-19 12:42:21 -0400

    finished stretch goal

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -41,6 +41,29 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
+    if len(arr) == 0:
+        return arr
 
+    # if maximum is not given, we'll take the max value from the input array
+    if maximum == -1:
+        maximum = max(arr)
+
+    # make a bunch of counter       always include +1
+    counter = [0] * ( maximum + 1 )
+    # counter = [0 for _ in range(maximum+1)] # this works as well
+
+    for i in arr:
+        if i < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        counter[i] += 1
+
+    # we have the counts of every value in our input array
+    # loop through our buckets, starting at the smallest index
+    ndx = 0
+    for i in range( len( counter ) ):
+        while 0 < counter[i]:
+            arr[ndx] = i
+            ndx += 1
+            counter[i] -= 1
 
     return arr
